a56ef6fbeab981098f753e06db764ee0aa37356f
knukio <hiro5051@gmail.com>
2018-03-03 00:36:52 +0900

    add input validation

--- exe/main.rb
+++ exe/main.rb
@@ -1,15 +1,33 @@
 require "FizzBuzzLocal"
 include FizzBuzzLocal
 
-MIN = (ARGV[0] || "1").to_i    # default = 1
-MAX = (ARGV[1] || "100").to_i  # default = 100
+def s_to_i(s)
+  if s =~ /^[0-9]+$/
+    s.to_i
+  else
+    raise "argument '#{s}' should be integer"
+  end
+end
+
+DEFAULT_MIN = "1"
+DEFAULT_MAX = "100"
+
+# validate arguments
+if !ARGV.size.between?(0, 2)
+  raise "the number of arguments should be between 0 and 2"
+end
+
+min = s_to_i(ARGV[0] || DEFAULT_MIN)
+max = s_to_i(ARGV[1] || DEFAULT_MAX)
+
+if(min > max)
+  raise "first argument should be smaller than second argument"
+end
 
 CONVERSION_TABLE = {
   3 => "Fizz",
   5 => "Buzz"
 }
 
-numbers = Array(MIN..MAX)
+numbers = Array(min..max)
 puts convert_numbers(numbers, CONVERSION_TABLE).join(' ')
-
-
